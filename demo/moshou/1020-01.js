5f62f8eba150e8c9a3e601f6d27a1445ff63b3f7
unknown <1476138456@qq.com>
2017-12-19 16:21:24 +0800

    Site updated: 2017-12-19 16:21:18

--- demo/moshou/1020-01.js
+++ demo/moshou/1020-01.js
@@ -50,55 +50,7 @@ var scrollFunc = function (e) {
     if (e.wheelDelta) { //判断浏览器IE，谷歌滑轮事件
         if (e.wheelDelta > 0) { //当滑轮向上滚动时
             var scrollTop = $(document).scrollTop();
-            if(scrollTop <= 100){
-                $(".page_home_con_nav").animate({
-                    width: "1380px",
-                    height:"90px"
-                })
-                $(".page_home_con_nav .con_nav_list_ul,.page_home_con_nav .con_right_list_item ,.page_home_con_nav .con_right_list").animate({
-                    lineHeight:"90px",
-                    height:"90px",
-//                            border:"1"
-                })
-                $(".con_right_list_item02").animate({
-                    marginTop:"35px"
-                })
-                $(".page_home_con_pic").animate({
-                    width:"293px",
-                    height:"118px"
-                }).css({
-                    background:"url(Logo-wow-cn.png) no-repeat",
-                    backgroundSize:"100% 100%"
-                })
-            }
-        }
-        if (e.wheelDelta < 0) { //当滑轮向下滚动时
-            var scrollTop = $(document).scrollTop();
-            if(scrollTop == 0){
-                $(".page_home_con_nav").animate({
-                    width: "100%",
-                    height:"50px"
-                })
-                $(".page_home_con_nav .con_nav_list_ul,.page_home_con_nav .con_right_list_item ,.page_home_con_nav .con_right_list").animate({
-                    lineHeight:"50px",
-                    height:"50px",
-                    borderLeft:"1px solid rgba(68,47,38,.5);"
-                })
-                $(".con_right_list_item02").animate({
-                    marginTop:"15px"
-                })
-                $(".page_home_con_pic").animate({
-                    width:"50px",
-                    height:"50px"
-                }).css({
-                    background:"url(Logo-wowIcon.png) no-repeat 10px 5px",
-                    backgroundSize:"40px 40px"
-                })
-            }
-
-        }
-    } else if (e.detail) { //Firefox滑轮事件
-        if (e.detail > 0) { //当滑轮向上滚动时 浏览器认为是向下滚动
+            if(scrollTop <= 100){="" $(".page_home_con_nav").animate({="" width:="" "1380px",="" height:"90px"="" })="" $(".page_home_con_nav="" .con_nav_list_ul,.page_home_con_nav="" .con_right_list_item="" ,.page_home_con_nav="" .con_right_list").animate({="" lineheight:"90px",="" height:"90px",="" border:"1"="" $(".con_right_list_item02").animate({="" margintop:"35px"="" $(".page_home_con_pic").animate({="" width:"293px",="" height:"118px"="" }).css({="" background:"url(logo-wow-cn.png)="" no-repeat",="" backgroundsize:"100%="" 100%"="" }="" if="" (e.wheeldelta="" <="" 0)="" {="" 当滑轮向下滚动时="" var="" scrolltop="$(document).scrollTop();" if(scrolltop="=" 0){="" "100%",="" height:"50px"="" lineheight:"50px",="" height:"50px",="" borderleft:"1px="" solid="" rgba(68,47,38,.5);"="" margintop:"15px"="" width:"50px",="" background:"url(logo-wowicon.png)="" no-repeat="" 10px="" 5px",="" backgroundsize:"40px="" 40px"="" else="" (e.detail)="" firefox滑轮事件="" (e.detail=""> 0) { //当滑轮向上滚动时 浏览器认为是向下滚动
 
         }
         if (e.detail < 0) { //当滑轮向下滚动时 浏览器认为是向上滚动
@@ -113,27 +65,4 @@ if (document.addEventListener) {
 //滚动滑轮触发scrollFunc方法
 document.onmousewheel = scrollFunc;
 
-for(i=0;i<pic.length;i++){
-    pic[i].index = i;
-    pic[i].onmouseover = function () {
-        var index = this.index;
-        for(var j=0;j<cheek.length;j++){
-            cheek[j].style.border = "1px solid transparent";
-            test_color[j].style.color = "#f8b700";
-        }
-        cheek[index].style.border = "1px solid #f8b700";
-        test_color[index].style.color = "#ffffff";
-    }
-}
-    left.onmouseover = function (){
-        left.style.border = "1px solid #f8b700";
-    }
-    left.onmouseout = function (){
-        left.style.border = "1px solid #504137";
-    }
-    right.onmouseover = function (){
-        right.style.border = "1px solid #f8b700";
-    }
-    right.onmouseout = function (){
-        right.style.border = "1px solid #504137";
-    }
+for(i=0;i</=>
